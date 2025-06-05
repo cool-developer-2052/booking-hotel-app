e9eba009c19e0eda0e91d5d647a0eef59c7f3c20
cool-developer-2052 <wpcontent97@gmail.com>
2025-06-05 17:07:39 +0330

    feat(header): add destination input field
    - Add text input for entering destination
    - Create local state to store user input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { MdLocationOn } from "react-icons/md";
+
 function Header() {
-  return <header className="header"></header>;
+  // === State for input field ===
+  const [destination, setDestination] = useState("");
+
+  return (
+    <header className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            className="headerSearchInput"
+            type="text"
+            name="destination"
+            id="destination"
+            placeholder="Where to go?"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <span className="seperator"></span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
