859beeaede7cf3ecc079855f5957de090a2ca288
cool-developer-2052 <wpcontent97@gmail.com>
2025-06-07 13:51:50 +0330

    feat(appLayout): create AppLayout component and route
    - Create shared layout to display hotels with map
    - Register route to render AppLayout as main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header.jsx";
 import LocationList from "./components/LocationList/LocationList.jsx";
-import { Routes, Route } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout.jsx";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/AppLayout/AppLayout.jsx
@@ -0,0 +1,10 @@
+function AppLayout() {
+  return (
+    <div className="applayout">
+      <h2>App Layout</h2>
+      <div className="sidebar">{/* Hotels */}</div>
+      {/* Map */}
+    </div>
+  );
+}
+export default AppLayout;
